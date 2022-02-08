73abd50ffe86580a5253d1774be78b4c4d5728f2
Dan Bornside <dan.bornside@gmail.com>
2022-02-07 19:28:36 -0500

    cleanSource

--- default.nix
+++ default.nix
@@ -2,6 +2,6 @@
 let pkgs = nixpkgs.pkgs;
     ghc = nixpkgs.pkgs.haskell.packages.${compiler};
     callCabal2nix = ghc.callCabal2nix;
-    hewwo = callCabal2nix "hewwo" ./. {};
+    hewwo = callCabal2nix "hewwo" (nixpkgs.lib.cleanSource ./.) {};
 in hewwo
 
