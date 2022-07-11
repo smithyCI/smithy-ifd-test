1790835553d192b39a609e42d4d164625d11a4ca
Dan Bornside <dan.bornside@gmail.com>
2022-07-11 10:29:39 -0400

    depend on timestamp.txt as hard as possible

--- Main.hs
+++ Main.hs
@@ -1,7 +1,14 @@
+{-# LANGUAGE TemplateHaskell #-}
 module Main where
 
+import Language.Haskell.TH.Syntax
+import Control.Monad.IO.Class (liftIO)
+
+memeTime :: String
+memeTime = $(liftString =<< liftIO (readFile "timestamp.txt"))
+
 main :: IO ()
-main = putStrLn "Sup got any memes? :3"
+main = putStrLn ( "Sup got any memes? :3" <> memeTime)
 
 main2 :: IO ()
 main2 = putStrLn "aa"
